023ced3e9117d7cc5b796d34ff38b5cc80c9d197
Taniksha Datar <82774150+taniksha19@users.noreply.github.com>
2023-05-25 16:57:59 +0530

    Update chatbot.py

--- Codes/AI-5/chatbot.py
+++ Codes/AI-5/chatbot.py
@@ -1,26 +1,76 @@
+import re
 import random
+# Define the chatbot's rules and responses
+rules = {
+    'greeting': {
+        'patterns': [r'hello', r'hi', r'hey'],
+        'responses': ['Hello! Welcome to our food ordering service.', 'Hi there! How can I assist you with your order?']
+    },
+    'menu': {
+        'patterns': [r'menu', r'options'],
+        'responses': ['Sure! Here is our menu: ...\n 1.Pav Bhaji \n 2.Vada Pav \n 3.Maggi \n 4.Chai'],
+    },
+    'food items' : {
+        'patterns' : [r'Vada Pav',r'Pav Bhaji',r'Chai',r'Maggi' ],
+        'responses' : ['Do you want to confirm your order??'],
+    },
+    'order': {
+        'patterns': [r'order', r'I want to order'],
+        'responses': ['Great! Please let me know what items you would like to order.'],
+    },
+    'customization': {
+        'patterns': [r'customize', r'special request'],
+        'responses': ['Certainly! Let me know your specific requirements or any dietary restrictions.'],
+    },
+    'confirm_order': {
+        'patterns': [r'confirm', r'place order'],
+        'responses': ['Perfect! Your order has been placed. The estimated delivery time is approximately 30 minutes.'],
+    },
+    'gratitude' : {
+        'patterns' : [r'thank you'],
+        'responses' : ['My pleasure']
+    },
+    'cancel_order': {
+        'patterns': [r'cancel', r'change', r'update'],
+        'responses': ['I apologize for the inconvenience. Please contact our customer support for order modifications.'],
+    },
+    'goodbye': {
+        'patterns': [r'bye', r'goodbye', r'see you'],
+        'responses': ['Thank you for choosing our food ordering service. Ab nikal BC yaha se', 'Goodbye!'],
+    },
+    'default': {
+        'responses': ['Im sorry, I didnt understand that. Can you please rephrase?']
+    }
+}
 
-greetings = ["Hello","Hi","Hey There"]
-goodbyes = ["Bye", "See you later","Adios Amigos"]
-help_responses = ["How may I help you?", "How can I assist you?", "What can I do for you?"]
-problem_responses = ["I'm so sorry to hear that. Can you please tell me more about the problem?", "Let me see if I can help fix your issue.", "I'll do my best to help you"]
-thankyou_responses = ["You are welcome", "It was a pleasure", "Glad to be of some help"]
+# Function to match user input with patterns
+def match_patterns(user_input, patterns):
+    for pattern in patterns:
+        match = re.search(pattern, user_input, re.IGNORECASE)
+        if match:
+            return True
+    return False
 
-def chatbot():
-    print("Chatbot: " + random.choice(greetings))
+# Function to get chatbot's response
+def get_response(user_input):
+    for intent, data in rules.items():
+        patterns = data.get('patterns')
+        if patterns and match_patterns(user_input, patterns):
+            responses = data.get('responses')
+            return random.choice(responses)
+    return random.choice(rules['default']['responses'])
+
+# Main conversation loop
+def chat():
+    print("Chatbot: Hello! Welcome to our food ordering service.")
     while True:
         user_input = input("User: ")
-        if 'hello' in user_input.lower() or "hi" in user_input.lower() or 'hey' in user_input.lower():
-            print("Chatbot: " + random.choice(greetings))
-        elif 'bye' in user_input.lower() or 'goodbye' in user_input.lower() or 'see you' in user_input.lower():
-            print("Chatbot: " + random.choice(goodbyes))
-        elif 'help' in user_input.lower() or 'support' in user_input.lower():
-            print("Chatbot: " + random.choice(help_responses))
-        elif 'problem' in user_input.lower() or 'issue' in user_input.lower() or 'error' in user_input.lower():
-            print("Chatbot: " + random.choice(problem_responses))
-        elif 'thank you' in user_input.lower() or "thanks" in user_input.lower() or "thankyou" in user_input.lower():
-            print("Chatbot: " + random.choice(thankyou_responses))
-        else:
-            print("Chatbot: I'm sorry, I don't understand. Can you please rephrase your request?")
+        response = get_response(user_input)
+        print("Chatbot:", response)
 
-chatbot()
+        # Exit the loop if the user says goodbye
+        if any(re.search(pattern, user_input) for pattern in rules['goodbye']['patterns']):
+            break
+
+# Start the chatbot
+chat()
