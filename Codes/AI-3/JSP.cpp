a2ef57533ca437cab929beb24029ab73278bae6b
Taniksha Datar <82774150+taniksha19@users.noreply.github.com>
2023-05-25 19:30:23 +0530

    Update and rename JSP to JSP.cpp

---
+++ Codes/AI-3/JSP.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// A structure to represent a job
+struct Job {
+    char id;    // Job Id
+    int dead;   // Deadline of job
+    int profit; // Profit if job is completed before or on the deadline
+};
+
+// Comparator function for sorting jobs
+bool comparison(Job a, Job b) {
+    return (a.profit > b.profit);
+}
+
+// Returns maximum profit from jobs
+void printJobScheduling(vector<Job>& jobs) {
+    int n = jobs.size();
+
+    // Sort jobs according to decreasing order of profit
+    sort(jobs.begin(), jobs.end(), comparison);
+
+    vector<int> result(n);  // To store the sequence of jobs
+    vector<bool> slot(n, false); // To keep track of free time slots
+
+    // Iterate through all given jobs
+    for (int i = 0; i < n; i++) {
+        // Find a free slot for this job (Note that we start
+        // from the last possible slot)
+        for (int j = min(n, jobs[i].dead) - 1; j >= 0; j--) {
+            // Free slot found
+            if (slot[j] == false) {
+                result[j] = i;      // Add this job to the result
+                slot[j] = true;     // Make this slot occupied
+                break;
+            }
+        }
+    }
+
+    // Print the result
+    cout << "Job Sequence: ";
+    for (int i = 0; i < n; i++) {
+        if (slot[i])
+            cout << jobs[result[i]].id << " ";
+    }
+}
+
+int main() {
+    int numJobs;
+    cout << "Enter the number of jobs: ";
+    cin >> numJobs;
+
+    vector<Job> jobs(numJobs);
+
+    // Input job details from the user
+    for (int i = 0; i < numJobs; i++) {
+        cout << "Enter details for Job " << i + 1 << ":\n";
+        cout << "  ID: ";
+        cin >> jobs[i].id;
+        cout << "  Deadline: ";
+        cin >> jobs[i].dead;
+        cout << "  Profit: ";
+        cin >> jobs[i].profit;
+    }
+
+    // Call the function to print the maximum profit sequence of jobs
+    cout << "Maximum profit sequence of jobs: ";
+    printJobScheduling(jobs);
+
+    return 0;
+}
+In this updated code, the user is prompted to enter the number of jobs. Then, a loop is used to input the details of each job, including the job ID, deadline, and profit.
+
+After the user input, the printJobScheduling function is called to determine and print the maximum profit sequence of jobs.
+
+Now, users can enter the number of jobs and their details interactively when running the program. The program will output the maximum profit sequence of jobs based on the provided input.
+
+
+
+
+
+
+
